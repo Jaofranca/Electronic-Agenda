1176117148c21ac49d315314b3915ea24261c5ff
joao <joaovfranca@hotmail.com>
2022-05-31 17:21:24 -0300

    Created the initial folders based on clean architecture and started building the Contact entity

---
+++ android/app/src/main/kotlin/com/example/agenda_eletronica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agenda_eletronica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Domain/entities/contact.dart
@@ -0,0 +1,15 @@
+class Contact {
+  final String? name;
+  final String? cellPhoneNumber;
+  final String? email;
+  final String? image;
+  // final Address
+  // final Reminder
+
+  Contact({
+    this.name,
+    this.cellPhoneNumber,
+    this.email,
+    this.image,
+  });
+}
